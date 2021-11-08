a8dc543170f6689e34ae09d69e5aca3c7d2546f5
Mr. Paul Ishaili <paulishaili@gmail.com>
2021-11-08 02:35:38 +0100

    Forward

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+// Listen to PORT
+app.listen(PORT, () => {
+  console.log(`Connected to http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("assets"));
+
+// Templating Engine
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/account", (req, res) => {
+  res.render("pages/account");
+});
