7db31fbdc3503b73986358a731d6fa2a95d957cf
Mr. Paul Ishaili <81128574+wegiveyoubrand@users.noreply.github.com>
2021-11-09 15:18:30 +0100

    Now

---
+++ README.md
@@ -0,0 +1,2 @@
+# GigHire
+ Looking for a skill that you can't even find in the marketplace, and you are ready to pay? Here is the platform for you.
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Listen to PORT
 app.listen(PORT, () => {
@@ -15,11 +15,9 @@ app.use(express.static("assets"));
 // Templating Engine
 app.set("view engine", "ejs");
 
+// Router
+
 // Routes
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/account", (req, res) => {
-  res.render("pages/account");
 });
