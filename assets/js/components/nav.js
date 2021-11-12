e39b3bb9fba801845797fafe99a55431c96cf8eb
Mr. Paul Ishaili <81128574+wegiveyoubrand@users.noreply.github.com>
2021-11-12 22:11:04 +0100

    New

---
+++ assets/js/components/nav.js
@@ -0,0 +1,44 @@
+const navContainer = document.querySelector(".nav-list");
+const triggerOpens = document.querySelectorAll(".trigger-open");
+const triggerCloses = document.querySelectorAll(".trigger-close");
+const triggerToggles = document.querySelectorAll(".trigger-toggle");
+
+triggerOpens.forEach((triggerOpen) => {
+  triggerOpen.addEventListener("click", () => {
+    toggleTrigger(navContainer);
+  });
+});
+triggerToggles.forEach((triggerToggle) => {
+  triggerToggle.addEventListener("click", () => {
+    toggleTrigger(navContainer);
+  });
+});
+triggerCloses.forEach((triggerClose) => {
+  triggerClose.addEventListener("click", () => {
+    closeTrigger(navContainer);
+  });
+});
+
+function openTrigger(elem) {
+  if (elem.classList.contains("close")) {
+    elem.classList.remove("close");
+  }
+  elem.classList.add("open");
+}
+
+function closeTrigger(elem) {
+  if (elem.classList.contains("open")) {
+    elem.classList.remove("open");
+  }
+  elem.classList.add("close");
+}
+
+function toggleTrigger(elem) {
+  if (elem.classList.contains("open")) {
+    elem.classList.remove("open");
+    elem.classList.add("close");
+  } else {
+    elem.classList.remove("close");
+    elem.classList.add("open");
+  }
+}
